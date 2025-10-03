326ec7362a00834bdb9d9b2edd0383e94ad81ce2
glitchdetected <braveraptor99@gmail.com>
2025-10-03 18:33:42 -0400

    hmm

--- web/next.config.js
+++ web/next.config.js
@@ -1,69 +1,27 @@
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.discordapp.com',
-        pathname: '/avatars/**',
-      },
-            {
-                protocol: "https",
-                hostname: "cdn.discordapp.com",
-                port: "",
-                pathname: "/**"
-            },
-            {
-                protocol: "https",
-                hostname: "imageapi.notificationbot.xyz",
-                port: "",
-                pathname: "/"
-            },
-                        {
-                protocol: "https",
-                hostname: "yt3.ggpht.com",
-                port: "",
-                pathname: "/**"
-            },
-            {
-                protocol: "https",
-                hostname: "static-cdn.jtvnw.net",
-                port: "",
-                pathname: "/jtv_user_pictures/**"
-            },
-           {
-               protocol: "https",
-               hostname: "cdn.bsky.app",
-               port: "",
-               pathname: "/img/avatar/plain/**"
-           },
-           {
-               protocol: "https",
-               hostname: "www.redditstatic.com",
-               port: "",
-               pathname: "/avatars/**"
-           },
-           {
-               protocol: "https",
-               hostname: "upload.wikimedia.org",
-               port: "",
-               pathname: "/**"
-           },
-           {
-               protocol: "https",
-               hostname: "cdn.bsky.app",
-               port: "",
-               pathname: "/**"
-           },
-            {
-                protocol: "http",
-                hostname: "localhost",
-                port: "5000",
-                pathname: "/**"
-            },
+      { protocol: 'https', hostname: 'cdn.discordapp.com', pathname: '/avatars/**' },
+      { protocol: 'https', hostname: 'cdn.discordapp.com', port: '', pathname: '/**' },
+      { protocol: 'https', hostname: 'imageapi.notificationbot.xyz', port: '', pathname: '/' },
+      { protocol: 'https', hostname: 'yt3.ggpht.com', port: '', pathname: '/**' },
+      { protocol: 'https', hostname: 'static-cdn.jtvnw.net', port: '', pathname: '/jtv_user_pictures/**' },
+      { protocol: 'https', hostname: 'cdn.bsky.app', port: '', pathname: '/img/avatar/plain/**' },
+      { protocol: 'https', hostname: 'www.redditstatic.com', port: '', pathname: '/avatars/**' },
+      { protocol: 'https', hostname: 'upload.wikimedia.org', port: '', pathname: '/**' },
+      { protocol: 'https', hostname: 'cdn.bsky.app', port: '', pathname: '/**' },
+      { protocol: 'http', hostname: 'localhost', port: '5000', pathname: '/**' },
     ],
+  },
+  webpack: (config) => {
+    if (!config.resolve.plugins) config.resolve.plugins = [];
+    config.resolve.plugins.push(new TsconfigPathsPlugin());
+    return config;
   },
 };
 
-module.exports = (nextConfig);
+export default nextConfig;
